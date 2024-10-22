0b165e2673d5b2d204daee8bc17000534d7f43d1
Minh-Nguyen521 <minhb2306674@student.ctu.edu.vn>
2024-10-22 12:24:24 +0700

    feat: revert

--- examples/chain/26_MsgConfigCron/example.go
+++ examples/chain/26_MsgConfigCron/example.go
@@ -53,7 +53,6 @@ func main() {
 		"user1",
 		kr,
 	)
-
 	if err != nil {
 		panic(err)
 	}
@@ -76,34 +75,7 @@ func main() {
 		Strategy: cronBinary,
 		Query: &types.FISQueryRequest{
 			// track balances of accounts you want to make the amount even
-			Instructions: []*types.FISQueryInstruction{
-				{
-					Plane: types.Plane_COSMOS,
-					Action: types.QueryAction_COSMOS_QUERY,
-					Address: []byte{},
-					Input: [][]byte{
-						[]byte(`
-						{
-							"rewards": [
-							  {
-								"validator_address": "luxvaloper1qry5x2d383v9hkqc0fpez53yluyxvey2c957m4",
-								"reward": [
-								  {
-									"denom": "lux",
-									"amount": "1234567800000.000000000000000000"
-								  }
-							  }
-							],
-							"total": [
-							  {
-								"denom": "lux",
-								"amount": "1234567800000.000000000000000000"
-							  }
-							]
-						}`),
-					},		
-				},
-			},
+			Instructions: []*types.FISQueryInstruction{},
 		},
 		TriggerPermission: &strategytypes.PermissionConfig{
 			Type:      strategytypes.AccessType_only_addresses,
@@ -118,7 +90,7 @@ func main() {
 			Tags:         []string{"cron", "bank", "util"},
 			Schema:       "",
 			CronGasPrice: math.NewIntFromUint64(500000000),
-			CronInput:    `{}`,
+			CronInput:    `{"receiver":"lux158ucxjzr6ccrlpmz8z05wylu8tr5eueqcp2afu","amount":"1","denom":"lux"}`,
 			CronInterval: 2,
 		},
 	}
